e321b138e6aea06b8d3b449d5a5bd128f0a4881f
tjforeman <tylerforeman92@gmail.com>
2019-11-08 13:58:44 -0500

    robot passing tests

--- robot_sort/robot_sort.py
+++ robot_sort/robot_sort.py
@@ -97,7 +97,30 @@ class SortingRobot:
         Sort the robot's list.
         """
         # Fill this out
-        pass
+
+        while self.light_is_on() == False:
+            self.set_light_on()
+
+            while self.can_move_right():
+                self.swap_item()
+                self.move_right()
+
+                if self.compare_item() == 1:
+                    self.swap_item()
+                    self.move_left()
+                    self.swap_item()
+                    self.move_right()
+                    self.set_light_off()
+
+                else:
+                    self.move_left()
+                    self.swap_item()
+                    self.move_right()
+
+            while self.can_move_left():
+                self.move_left()
+
+
 
 
 if __name__ == "__main__":
